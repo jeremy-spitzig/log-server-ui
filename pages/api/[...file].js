91e8c94417889c14acd5c1a98f36329096755d0c
jeremy-spitzig <jpspitzig@gmail.com>
2021-01-08 12:45:52 -0300

    feat: add api route for retrieving logs

---
+++ pages/api/[...file].js
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async (req, res) => {
+  let requestUrl = process.env.LOG_SERVER
+  if(!requestUrl.endsWith('/')) {
+    requestUrl += '/'
+  }
+  requestUrl += req.query.file.join('/')
+  let params = []
+  if(req.query.n) {
+    params.push('n=' + req.query.n)
+  }
+  if(req.query.filter) {
+    params.push('filter=' + req.query.filter)
+  }
+  if(params.length > 0) {
+    requestUrl += '?' + params.join('&')
+  }
+  const result = await fetch(requestUrl)
+  const text = await result.text()
+  console.log(text)
+  res.statusCode = 200
+  res.json({
+    text
+  })
+}
